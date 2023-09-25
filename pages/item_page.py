f8d3ecdc33a45f14ab04747d4dcc4f0380ced6a4
Lsee Iv <Le7ya07@mail.ru>
2023-09-25 14:36:03 +0700

    Add new pages

--- pages/item_page.py
+++ pages/item_page.py
@@ -1,3 +1,4 @@
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -14,16 +15,29 @@ class Item_page(Base):
 
     # Locators
 
-    item_button = "//span[@class='header-catalogBtn unselectable']"2
+    item_button = "//span[@class='header-catalogBtn unselectable']"
 
+    select_catalog = "//a[@title='Трикотаж']"
+    select_category = "//a[@title='Платья']"
 
-    select_product_1 = "//button[@id='add-to-cart-sauce-labs-backpack']"
-    select_product_2 = "//button[@id='add-to-cart-sauce-labs-bike-light']"
-    cart = "//div[@id='shopping_cart_container']"
-    menu = "//button[@id='react-burger-menu-btn']"
-    link_about = "//a[@id='about_sidebar_link']"
+    select_product_1 = "//a[@title='Платье 24652 [красный]']"
+    select_product_2 = "//a[@title='Платье 59208 [молочный]']"
+    add_to_cart_product_1 = "//button[@title='Положить «Платье 24652 [красный]» в корзину']"
+    add_to_cart_product_2 = "//button[@title='Положить «Платье 59208 [молочный]» в корзину']"
+    close_window = "//a[@class='fancybox-item fancybox-close']"
+    cart = "//span[@class='header-toolsIcon _cart']"
+
 
     # Getters
+
+    def get_item_button(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.item_button)))
+
+    def get_select_catalog(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_catalog)))
+
+    def get_select_category(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_category)))
 
     def get_select_product_1(self):
         return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_product_1)))
@@ -31,64 +45,79 @@ class Item_page(Base):
     def get_select_product_2(self):
         return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_product_2)))
 
-    def get_select_product_3(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_product_3)))
+    def get_cart_product_1(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.add_to_cart_product_1)))
+
+    def get_cart_product_2(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.add_to_cart_product_2)))
 
     def get_cart(self):
         return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.cart)))
-
-    def get_menu(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.menu)))
-
-    def get_link_about(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.link_about)))
+    def get_close_window(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.close_window)))
 
 
     # Actions
 
+    def move_to_item(self):
+        actions = ActionChains(self.driver)
+        actions.move_to_element(self.get_item_button())
+        actions.perform()
+        print("Навели курсор на список товаров")
+
+    def move_to_catalog(self):
+        actions = ActionChains(self.driver)
+        actions.move_to_element(self.get_select_catalog())
+        actions.perform()
+        print("Навели курсор на категорию в каталоге")
+
+    def move_to_category(self):
+        actions = ActionChains(self.driver)
+        actions.click(self.get_select_category())
+        actions.perform()
+        print("Нажали на определенную категорию")
+
     def click_select_product_1(self):
         self.get_select_product_1().click()
-        print("Click select product 1")
+        print("Выбрали продукт 1")
 
     def click_select_product_2(self):
         self.get_select_product_2().click()
-        print("Click select product 2")
+        print("Выбрали продукт 2")
 
-    def click_select_product_3(self):
-        self.get_select_product_3().click()
-        print("Click select product 3")
+    def back_to(self):
+        self.driver.back()
+        print("Вернулись назад")
+
+    def scroll_to(self):
+        self.driver.execute_script("window.scrollTo(0, 1500)")
+        print("Проскролили")
+
+    def add_to_cart_prod_1(self):
+        self.get_cart_product_1().click()
+        print("Добавили в корзину продукт 1")
+
+    def add_to_cart_prod_2(self):
+        self.get_cart_product_2().click()
+        print("Добавили в корзину продукт 2")
 
     def click_cart(self):
         self.get_cart().click()
-        print("Click cart")
+        print("Открыли корзину")
 
-    def click_menu(self):
-        self.get_menu().click()
-        print("Click menu")
-
-    def click_link_about(self):
-        self.get_link_about().click()
-        print("Click link about")
+    def click_close_window(self):
+        self.get_close_window().click()
+        print("Закрыли модальное окно")
 
     # Methods
-
     def select_products_1(self):
         self.get_current_url()
+        self.move_to_item()
+        self.move_to_catalog()
+        self.move_to_category()
         self.click_select_product_1()
-        self.click_cart()
 
     def select_products_2(self):
         self.get_current_url()
         self.click_select_product_2()
         self.click_cart()
-
-    def select_products_3(self):
-        self.get_current_url()
-        self.click_select_product_3()
-        self.click_cart()
-
-    def select_menu_about(self):
-        self.get_current_url()
-        self.click_menu()
-        self.click_link_about()
-        self.assert_url("https://saucelabs.com/")
--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -3,16 +3,13 @@ import time
 import pytest
 from selenium.webdriver.support import expected_conditions as EC
 
-from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
+from pages.item_page import Item_page
 from pages.login_page import Login_page
-import sys
 
 
-# sys.path.append("C:\\Users\\Ivandaeva\\PycharmProjects\\main_project")
 
 # @pytest.mark.run(order=3)
 def test_buy_product_1():
@@ -23,7 +20,22 @@ def test_buy_product_1():
     login = Login_page(driver)
     login.authorization()
 
+    ip = Item_page(driver)
+    ip.move_to_category()
+    ip.click_select_product_1()
+    ip.add_to_cart_prod_1()
+    ip.click_close_window()
+
+    ip.back_to()
+    ip.scroll_to()
+    ip.click_select_product_2()
+    ip.add_to_cart_prod_2()
+    ip.click_close_window()
+    ip.click_cart()
+
+
+
 
     print("Finish test 1")
-    time.sleep(10)
+    time.sleep(5)
     driver.quit()
